300988fc004b51293deae541518561858dfbb1f7
Aesthetic <117440607+Scarbete@users.noreply.github.com>
2023-05-30 22:12:18 +0600

    scarbete

---
+++ Ty_Kto_Takoi/src/store/todoReducer.jsx
@@ -0,0 +1,40 @@
+import axios from "axios"
+
+const initialState = {todos: []}
+
+// const todoReducer = (state=initialState, action) => {
+//     if (action.type == 'ADD_TODO') {
+//         return {...state, todos: [...state.todos, action.payload]}
+//     }
+//     else if (action.type == 'REMOVE_TODO') {
+//         return {...state, todos: state.todos.filter((todo) => todo.id !== action.payload)}
+//     }
+//     else if (action.type == 'SET_TODOS') {
+//         return {...state, todos: action.payload}
+//     }
+//     return state
+// }
+
+const todoReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return { ...state, todos: [...state.todos, action.payload]}
+        case 'REMOVE_TODO':
+            return { ...state, todos: state.todos.filter((todo) => todo.id !== action.payload)}
+        case 'SET_TODOS':
+            return { ...state, todos: action.payload }
+        default:
+            return state
+    }
+}
+
+export const fetchTodos = () => {
+    return async (dispatch) => {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then(resp => dispatch({type: 'SET_TODOS', payload: resp.data}))
+    }
+}
+
+export const addTodo = payload => ({type: "ADD_TODO", payload})
+export const deleteTodo = payload => ({type: "REMOVE_TODO", payload})
+export default todoReducer
